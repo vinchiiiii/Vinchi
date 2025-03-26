4ad0e6c1956d799e835a292a28337eaf7cd33364
vinchiiiii <vincentbarona12@gmail.com>
2025-03-27 00:26:02 +0800

    Add files via upload

---
+++ Index html.py
@@ -0,0 +1,16 @@
+def create_message(friend_name):
+    message = f"""Hey {friend_name},
+
+I hope you are doing well. I know school has a workload of exams, assignments, and other things in between,
+so I totally get how busy everything can be. I hope everything is just as positive on your side.
+Whatever it is that you are working on, I wish you the best! Take care of yourself.
+
+Yo best friend,
+Vincent
+"""
+    print("\nYour message:\n")
+    print(message)
+
+
+if __name__ == "__main__":
+    create_message("Mah Bestfriend") # Jassy's name is now hardcoded here.
